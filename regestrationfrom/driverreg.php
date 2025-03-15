ffa4cc97665b3e169b6059ad959f6f3c54bb573f
hameem2003 <hameem007mustafa@gmail.com>
2025-03-16 05:45:23 +0600

    Resolved merge conflicts

--- regestrationfrom/driverreg.php
+++ regestrationfrom/driverreg.php
@@ -9,7 +9,7 @@
     <h1>Driver Registration Form</h1>
 
     <form action="register_process.php" method="POST">
-        <table border="1" cellpadding="5" cellspacing="0">
+        <table border="5" cellpadding="5" cellspacing="1">
             <tr>
                 <td>*Applicant's Name :</td>
                 <td><input type="text" name="applicant_name" required></td>
@@ -24,7 +24,10 @@
             </tr>
             <tr>
                 <td>*Nationality:</td>
-                <td><input type="text" name="nationality"></td>
+                <td><input type="checkbox" id="bangladeshi" name="nationality" value="Bangladeshi"> Bangladeshi
+                    <input type="checkbox" id="others" name="nationality" value="Others"> Others
+                     <input type="text" name="otherNationality" placeholder="Enter nationality">
+                </td>
             </tr>
             <tr>
                 <td>*Gender:</td>
@@ -38,27 +41,27 @@
                 <td>*Date of Birth:</td>
                 <td>
                     <input type="text" name="dob_day" placeholder="Day" size="5"> 
-                    <input type="text" name="dob_month" placeholder="months" size="5"> 
-                    <input type="text" name="dob_year" placeholder="year" size="7">
+                    <input type="text" name="dob_month" placeholder="Months" size="5"> 
+                    <input type="text" name="dob_year" placeholder="Year" size="7">
                 </td>
             </tr>
             <tr>
                 <td>*National ID:</td>
                 <td> 
-                    <input type="text" name="id_number" placeholder="Enter ID Number">
+                    <input type="text" name="id_number" id_num="Enter ID Number" required>
                 </td>
             </tr>
             <tr>
                 <td>*Driving License:</td>
                 <td>
                      
-                    <input type="text" name="id_number" placeholder="Enter ID Number">
+                    <input type="text" name="id_number" id_num="Enter ID Number" required>
                 </td>
             </tr>
             
             <tr>
-                <td>Driving Experience (Years):</td>
-                <td><input type="number" name="experience"  placeholder="Enter years of experience"></td>
+                <td>*Driving Experience (Years):</td>
+                <td><input type="number" name="experience"  placeholder="Enter years of experience" required></td>
             </tr>
             <tr>
                 <td>*Mobile Number:</td>
